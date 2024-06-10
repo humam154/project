4dde816bbad58234631963ec434c5b058d699489
Humam Allawi <humam.allawi.18@gmail.com>
2024-06-10 15:03:55 +0300

    added requests for auth

---
+++ app/Http/Requests/UserSignInRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Responses\Response;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class UserSignInRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'email|required',
+            'password' => 'required|string|min:8'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        Throw new ValidationException($validator, Response::Validation([], $validator->errors()));
+    }
+}
