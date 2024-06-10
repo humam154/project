ac5afb662d3d7a5376ded67e639a40dd8c0adbc1
Humam Allawi <humam.allawi.18@gmail.com>
2024-06-10 14:54:40 +0300

    added logout service, and admin login, and append roles service

--- app/services/UserService.php
+++ app/services/UserService.php
@@ -59,4 +59,67 @@ class UserService
 
         return ['user' => $user, 'message' => $message, 'code' => $code];
     }
+
+    public function logout(): array
+    {
+        $user = Auth::user();
+
+        if(!is_null($user)){
+            Auth::user()->currentAccessToken->delete();
+            $message = 'logged out successfully';
+            $code = 200;
+        }
+        else{
+            $message = 'invalid token';
+            $code = 404;
+        }
+
+        return ['user' => $user, 'message' => $message, 'code' => $code];
+    }
+
+    public function adminLogin($request): array
+    {
+        $user = User::query()
+            ->where('email', $request['email'])
+            ->first();
+
+        if(!is_null($user)){
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                $message = 'user email and password does not match our record';
+                $code = 401;
+            }
+            else{
+                $user = $this->appendRolesAndPermissions($user);
+                $user['token'] = $user->createToken("token")->plainTextToken;
+                $message = 'user logged in successfully';
+                $code = 200;
+            }
+        }
+        else {
+            $message = 'user not found';
+            $code = 404;
+        }
+
+        return ['user' => $user, 'message' => $message, 'code' => $code];
+    }
+
+    private function appendRolesAndPermissions($user){
+        $roles = [];
+
+        foreach ($user->roles as $role){
+            $roles[] = $role->name;
+        }
+
+        unset($user['roles']);
+        $user['roles'] = $roles;
+
+        $permissions = [];
+        foreach ($user->permissions as $permission){
+            $permissions[] = $permission->name;
+        }
+        unset($user['permissions']);
+        $user['permissions'] = $permissions;
+
+        return $user;
+    }
 }
