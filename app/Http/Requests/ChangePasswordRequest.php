d9b9ef1252f395d12c4c59a68b9d269b843a7c8a
Humam Allawi <humam.allawi.18@gmail.com>
2024-08-04 16:54:36 +0300

    removed imports, nothing important

--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Http\Responses\Response;
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\ValidationException;
@@ -20,7 +21,7 @@ class ChangePasswordRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
